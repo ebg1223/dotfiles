0c8e1400f0f139b433e32c731e06736bbda4dfa0
Ethan Greenfeld <egreenfeld@azswa.org>
2025-09-26 14:49:02 -0700

    Remove .local/share/devbox/global/default/devbox.json Remove .local/share/devbox/global/default/devbox.lock Remove .local/share/devbox/global/default/.bin/opencode Remove .local/share/devbox/global/default/.devbox/.gitignore Remove .local/share/devbox/global/default/.devbox/.nix-print-dev-env-cache Remove .local/share/devbox/global/default/.devbox/nix/profile/default Remove .local/share/devbox/global/default/.devbox/nix/profile/default-52-link Remove .local/share/devbox/global/default/.devbox/gen/flake/flake.lock Remove .local/share/devbox/global/default/.devbox/gen/flake/flake.nix Remove .local/share/devbox/global/default/.devbox/gen/scripts/.hooks.sh Remove .local/share/devbox/global/default/.devbox/gen/scripts/test.sh Remove .local/share/devbox/global/default/.devbox/gen/shell.nix Remove .local/share/devbox/global/default/.devbox/state.json Remove .local/share/devbox/global/default/.devbox/virtenv/nodejs/corepack-bin/.keep Remove .local/share/devbox/global/default/.devbox/virtenv/runx/bin/.keep

--- private_dot_local/private_share/devbox/global/default/dot_devbox/private_gen/scripts/executable_dot_hooks.sh
+++
@@ -1,3 +0,0 @@
-test -z $DEVBOX_COREPACK_ENABLED || corepack enable --install-directory "/Users/ethan/.local/share/devbox/global/default/.devbox/virtenv/nodejs/corepack-bin/"
-test -z $DEVBOX_COREPACK_ENABLED || export PATH="/Users/ethan/.local/share/devbox/global/default/.devbox/virtenv/nodejs/corepack-bin/:$PATH"
-echo 'Welcome to devbox!' > /dev/null
--- private_dot_local/private_share/devbox/global/default/dot_devbox/private_gen/scripts/executable_test.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-if [ -z "$__DEVBOX_SKIP_INIT_HOOK_49f7065302aa8a273dad0a88d05d6070d0d455d3e4ab1bb1d0f3dd23285b9cfc" ]; then
-    . "/Users/ethan/.local/share/devbox/global/default/.devbox/gen/scripts/.hooks.sh"
-fi
-
-echo "Error: no test specified" && exit 1
